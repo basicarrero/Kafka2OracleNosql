40c6a4cfb637265b85bfcf524c32e82210600ff0
Basilio Carrero Nevado <basilio.carrero@avanttic.com>
2019-02-17 18:08:16 +0100

    code movement

---
+++ src/Driver.java
@@ -0,0 +1,12 @@
+import avt.nosql.JsonUtil;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Driver {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 2 || args.length == 3)
+            System.out.println(JsonUtil.inferSchema(Paths.get(args[1]), args[0], (args.length == 3) ? args[2] : null));
+        else
+            System.out.println(JsonUtil.inferSchema(Paths.get("./sample2.json"), "coches", "matricula"));
+    }
+}
