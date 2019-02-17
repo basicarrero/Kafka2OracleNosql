7e83b7c6f0bcb87bd98a2d5f684cbd073baa608e
Basilio Carrero Nevado <basilio.carrero@avanttic.com>
2019-02-17 18:06:49 +0100

    remove old files

--- driver.groovy
+++
@@ -1,6 +0,0 @@
-import java.nio.file.Paths
-
-if (args.size() == 2 || args.size() == 3)
-    println(JsonUtil.inferSchema(Paths.get(args[1]), args[0], (args.size() == 3) ? args[2] : null))
-else
-    println(JsonUtil.inferSchema(Paths.get("./sample.json"), "coches", "matricula"))
